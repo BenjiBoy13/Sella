d34603f32d9f1c5e0f1977325cdcec5395fab0c4
BenjiBoy13 <ben99.planetatierra@gmail.com>
2020-04-17 18:37:45 -0500

    Fixed bundle directory check in assets copy command

--- Stella/Modules/Terminal/Procedure/AssetsProcedure.php
+++ Stella/Modules/Terminal/Procedure/AssetsProcedure.php
@@ -114,7 +114,7 @@ final class AssetsProcedure extends Procedure
         $stellaBundleDirPath = PROJECT_DIR_CLI . "/public/bundles/stella";
         $stellaAssetsDirPath = dirname(__DIR__, 3) . "/Resources/assets";
 
-        if (file_exists($stellaAssetsDirPath)) {
+        if (file_exists($stellaBundleDirPath)) {
             return array(
                 'format' => 'warning',
                 'message' => 'Stella assets already copied, delete stella directory under bundles to copy again'
