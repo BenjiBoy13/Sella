82761cd1b1be00a779adb6de80e283d5f2abc383
BenjiBoy13 <ben99.planetatierra@gmail.com>
2020-05-01 18:23:00 -0500

    Added ORM Configuration

--- Stella/Modules/ORM/StellaOrm.php
+++ Stella/Modules/ORM/StellaOrm.php
@@ -3,14 +3,72 @@
 
 namespace Stella\Modules\ORM;
 
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Tools\Setup;
+use Stella\Core\Configuration;
+use Stella\Exceptions\Core\Configuration\ConfigurationFileNotFoundException;
+use Stella\Exceptions\Core\Configuration\ConfigurationFileNotYmlException;
+
 /**
  * Class StellaOrm
  *
  * @author Benjamin Gil FLores
- * @version 0.1
+ * @version 0.2
  * @package Stella\Modules\ORM
  */
 class StellaOrm
 {
+    /**
+     * @var Configuration
+     */
+    private Configuration $configuration;
 
+    /**
+     * StellaOrm constructor.
+     */
+    public function __construct ()
+    {
+        $this->configuration = new Configuration();
+    }
+
+    /**
+     * @return EntityManager
+     * @throws ConfigurationFileNotFoundException
+     * @throws ConfigurationFileNotYmlException
+     * @throws DBALException
+     * @throws ORMException
+     */
+    public function getEntityManager (): EntityManager
+    {
+        $config = Setup::createAnnotationMetadataConfiguration(
+            array(PROJECT_DIR . "/src"),
+            true,
+            null,
+            null,
+            false
+        );
+
+        return EntityManager::create(DriverManager::getConnection($this->getDatabaseConfiguration()), $config);
+    }
+
+    /**
+     * @return array
+     * @throws ConfigurationFileNotFoundException
+     * @throws ConfigurationFileNotYmlException
+     */
+    private function getDatabaseConfiguration (): array
+    {
+        $configuration = $this->configuration->getConfigurationOfFile(PROJECT_DIR . '/config/orm.yml');
+
+        return array(
+            'dbname' => $configuration['dbname'],
+            'user' => $configuration['user'],
+            'password' => $configuration['password'],
+            'host' => $configuration['host'],
+            'driver' => $configuration['driver']
+        );
+    }
 }
